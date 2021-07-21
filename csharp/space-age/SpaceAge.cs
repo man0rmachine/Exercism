373972f3c6d2804c80933ac5483a468c26ccd3e8
man0rmachine <matthew.hussong@gmail.com>
2021-07-20 19:29:07 -0700

    Exercism C-Sharp Solution #6

---
+++ csharp/space-age/SpaceAge.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class SpaceAge
+{
+    public double time;
+    public SpaceAge(int seconds)
+    { time = seconds; }
+    public double OnEarth() { return time/31557600; }
+    public double OnMercury() { return OnEarth() / 0.2408467; }
+    public double OnVenus() { return OnEarth() / 0.61519726; }
+    public double OnMars() { return OnEarth() / 1.8808158; }
+    public double OnJupiter() { return OnEarth() / 11.862615; }
+    public double OnSaturn() { return OnEarth() / 29.447498; }
+    public double OnUranus() { return OnEarth() / 84.016846; }
+    public double OnNeptune() { return OnEarth() / 164.79132; }
+}
