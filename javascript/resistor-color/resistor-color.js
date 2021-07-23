0698f81aabbe075e7ae85699a92af8611469c2b6
man0rmachine <matthew.hussong@gmail.com>
2021-07-22 19:55:13 -0700

    Exercism JavaScript Solution #3

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,2 @@
+export const colorCode = (color) => { return COLORS.indexOf(color, COLORS); };
+export const COLORS = [ 'black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white' ];
