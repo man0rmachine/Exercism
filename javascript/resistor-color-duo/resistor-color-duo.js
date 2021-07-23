240e6e1aadb6ebf60dd6c8ef2920450c4fde89fc
man0rmachine <matthew.hussong@gmail.com>
2021-07-22 22:32:16 -0700

    Exercism JavaScript Solution #4

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,2 @@
+export const colors = { 'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5, 'blue': 6, 'violet': 7, 'grey': 8, 'white': 9};
+export const decodedValue = ([color1, color2]) => { return parseInt(`${colors[color1]}${colors[color2]}`); };
